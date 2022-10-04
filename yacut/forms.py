29284f6e3d9ccb1b6657ef753a718b8adec871d3
Виталий Насретдинов <nasretdinovs@yandex.ru>
2022-10-04 17:40:18 +0300

    Refactoring: forms, views, templates

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, URLField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms.validators import DataRequired, Length, Optional, Regexp
 
 
 class URL_mapForm(FlaskForm):
@@ -12,6 +12,11 @@ class URL_mapForm(FlaskForm):
     )
     custom_id = StringField(
         'Ваш вариант короткой ссылки',
-        validators=[Length(1, 16), Optional()]
+        validators=[
+            Length(1, 16),
+            Regexp('^[A-Za-z0-9]*$',
+                   message='Указано недопустимое имя для короткой ссылки'),
+            Optional()
+        ]
     )
     submit = SubmitField('Создать')
--- yacut/views.py
+++ yacut/views.py
@@ -1,8 +1,7 @@
 import random
 import string
-from http import HTTPStatus
 
-from flask import abort, flash, redirect, render_template
+from flask import flash, redirect, render_template
 
 from . import app, db
 from .forms import URL_mapForm
@@ -18,28 +17,26 @@ def get_unique_short_id():
 def index_view():
     """Страница для создания короткой ссылки."""
     form = URL_mapForm()
-    if form.validate_on_submit():
-        short_id = form.custom_id.data
-        if (short_id and
-                URL_map.query.filter_by(short=short_id).first() is not None):
-            flash(f'Имя {short_id} уже занято!')
-            return render_template('index.html', form=form)
-        if not short_id:
-            short_id = get_unique_short_id()
-        url_obj = URL_map(
-            original=form.original_link.data,
-            short=short_id
-        )
-        db.session.add(url_obj)
-        db.session.commit()
-        return render_template('index.html', form=form, short_id=short_id)
-    return render_template('index.html', form=form)
+    if not form.validate_on_submit():
+        return render_template('index.html', form=form)
+    short_id = form.custom_id.data
+    if (short_id and
+            URL_map.query.filter_by(short=short_id).first() is not None):
+        flash(f'Имя {short_id} уже занято!')
+        return render_template('index.html', form=form)
+    if not short_id:
+        short_id = get_unique_short_id()
+    url_obj = URL_map(
+        original=form.original_link.data,
+        short=short_id
+    )
+    db.session.add(url_obj)
+    db.session.commit()
+    return render_template('index.html', form=form, short_id=short_id)
 
 
 @app.route('/<string:short_id>')
 def redirect_view(short_id):
     """Переадресация с короткой на оригинальную ссылку."""
-    url_obj = URL_map.query.filter_by(short=short_id).first()
-    if url_obj is not None:
-        return redirect(url_obj.original)
-    abort(HTTPStatus.NOT_FOUND)
+    url_obj = URL_map.query.filter_by(short=short_id).first_or_404()
+    return redirect(url_obj.original)
